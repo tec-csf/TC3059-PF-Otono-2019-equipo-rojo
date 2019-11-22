3df83e6bf276c58650a54609e50add4e59d2d9c9
antony999k <lv999k@hotmail.com>
2019-11-22 16:40:33 -0600

    asfnasfkj

--- frontend/src/app/helpers/config.helper.ts
+++ frontend/src/app/helpers/config.helper.ts
@@ -1,6 +1,6 @@
 export class Config{
-  public static apiUserUrl = '35.238.172.158';
-  public static apiPostUrl = '104.154.162.186';
+  public static apiUserUrl = 'http://35.238.172.158';
+  public static apiPostUrl = 'http://104.154.162.186';
   //public static apiUserUrl = 'http://127.0.0.1:8000';
   //public static apiPostUrl = 'http://127.0.0.1:8001';
 }
