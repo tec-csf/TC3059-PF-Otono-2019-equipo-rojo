c3ced04e5207cb6805c592e569b83a0a47b71aca
antony999k <lv999k@hotmail.com>
2019-11-25 07:30:33 -0600

    :sparkles: video demo

--- frontend/src/app/helpers/config.helper.ts
+++ frontend/src/app/helpers/config.helper.ts
@@ -1,6 +1,6 @@
 export class Config{
-  public static apiUserUrl = 'http://34.67.122.166';
-  public static apiPostUrl = 'http://35.226.135.82';
-  //public static apiUserUrl = 'http://127.0.0.1:8000';
-  //public static apiPostUrl = 'http://127.0.0.1:8001';
+  //public static apiUserUrl = 'http://34.67.122.166';
+  //public static apiPostUrl = 'http://35.226.135.82';
+  public static apiUserUrl = 'http://127.0.0.1:8000';
+  public static apiPostUrl = 'http://127.0.0.1:8001';
 }
