fbc9dec69f27be06cacd9aa9c0034afd64185fe1
antony999k <lv999k@hotmail.com>
2019-11-24 23:41:29 -0600

    :sparkles: New front routes

--- frontend/src/app/helpers/config.helper.ts
+++ frontend/src/app/helpers/config.helper.ts
@@ -1,6 +1,6 @@
 export class Config{
-  public static apiUserUrl = 'http://35.238.172.158';
-  public static apiPostUrl = 'http://104.154.162.186';
+  public static apiUserUrl = 'http://34.67.122.166';
+  public static apiPostUrl = 'http://35.226.135.82';
   //public static apiUserUrl = 'http://127.0.0.1:8000';
   //public static apiPostUrl = 'http://127.0.0.1:8001';
 }
